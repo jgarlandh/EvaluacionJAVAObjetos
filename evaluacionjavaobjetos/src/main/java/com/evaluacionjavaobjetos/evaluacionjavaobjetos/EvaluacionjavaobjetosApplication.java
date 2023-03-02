ecd1ef70ac15f22aaffe4ce3e7ade375da96171f
Jesus Garland <jgarlandh@gmail.com>
2023-03-02 00:34:40 -0300

    Modificacion, se quitan lineas 3,4 y 10 asociadas a Springboot

--- evaluacionjavaobjetos/src/main/java/com/evaluacionjavaobjetos/evaluacionjavaobjetos/EvaluacionjavaobjetosApplication.java
+++ evaluacionjavaobjetos/src/main/java/com/evaluacionjavaobjetos/evaluacionjavaobjetos/EvaluacionjavaobjetosApplication.java
@@ -1,13 +1,9 @@
 package com.evaluacionjavaobjetos.evaluacionjavaobjetos;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class EvaluacionjavaobjetosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EvaluacionjavaobjetosApplication.class, args);
 	}
 
 }
