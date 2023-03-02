c54d85fc680cd06b4093e2b9785e86b5f664be92
Jesus Garland <jgarlandh@gmail.com>
2023-03-02 04:28:09 -0300

    Creacion logica opcion 1

--- evaluacionjavaobjetos/src/main/java/com/evaluacionjavaobjetos/evaluacionjavaobjetos/EvaluacionjavaobjetosApplication.java
+++ evaluacionjavaobjetos/src/main/java/com/evaluacionjavaobjetos/evaluacionjavaobjetos/EvaluacionjavaobjetosApplication.java
@@ -1,6 +1,7 @@
 package com.evaluacionjavaobjetos.evaluacionjavaobjetos;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 import com.evaluacionjavaobjetos.evaluacionjavaobjetos.models.Alumno;
@@ -63,6 +64,61 @@ public class EvaluacionjavaobjetosApplication {
 
 
 	public static void main(String[] args) {
-	}
 
+		Scanner input = new Scanner(System.in);
+
+		int userOption = 1;
+		while(userOption != 0){
+			do {
+				System.out.println("Bienvenido!");
+				System.out.println("Ingrese '1' si quiere ver el promedio por alumno");
+				System.out.println("Ingrese '2' si quiere guardar un curso");
+				System.out.println("Ingrese '3' si quiere guardar notas y evaluarlas");
+				System.out.println("Ingrese '0' para salir del menú");
+				System.out.print("Ingrese su opción: ");
+				userOption=input.nextInt();
+			} while (userOption < 0 || userOption > 3);
+
+			if(userOption == 1){
+
+				HashMap <String,ArrayList<Double>> notas = new HashMap<>();
+				int cantAlum;
+				int cantNotas;
+				System.out.print("Indique cantidad de alumnos a ingresar : ");
+				cantAlum = input.nextInt(); 
+				System.out.print("Indique cantidad de notas a ingresar : ");
+				cantNotas = input.nextInt();
+
+				for (int i = 1; i <= cantAlum; i++) {
+					
+					ArrayList<Double> notasAlumnos = new ArrayList<Double>(); 
+
+					Alumno alumno = new Alumno();
+
+					System.out.print("Ingrese nombre del alumno: ");
+					alumno.setNombre(input.nextLine());
+
+					for (int x = 1; x <= cantNotas; x++) {
+
+						System.out.print("Ingrese nota nro "+x+" del alumno "+alumno.getNombre()+" :");
+						notasAlumnos.add(Double.parseDouble(input.nextLine()));
+						
+					}
+
+					notas.put(alumno.getNombre(), notasAlumnos);
+
+				}
+
+				for(String i : notas.keySet()){
+				
+		            System.out.println("El promedio del alumno "+ i + " es de:" + promedioNotas(notas.get(i)));
+				}
+
+			}else if(userOption == 2){
+
+			}
+
+
+		}
+	}
 }
