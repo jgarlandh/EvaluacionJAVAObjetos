2d951911f3bff04cc0ebc0fb67fb904b6c5a93f8
Jesus Garland <jgarlandh@gmail.com>
2023-03-02 00:26:11 -0300

    Creacion Clase Persona

---
+++ evaluacionjavaobjetos/src/main/java/com/evaluacionjavaobjetos/evaluacionjavaobjetos/models/Persona.java
@@ -0,0 +1,26 @@
+package com.evaluacionjavaobjetos.evaluacionjavaobjetos.models;
+
+public class Persona {
+    public String nombre;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona [nombre=" + nombre + "]";
+    }
+
+}
