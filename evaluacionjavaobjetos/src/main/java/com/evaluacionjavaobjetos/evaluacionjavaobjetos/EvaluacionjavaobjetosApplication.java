ab28777ba6a777b74012695184721661c6751450
Jesus Garland <jgarlandh@gmail.com>
2023-03-02 00:24:18 -0300

    Primer commit e inicio de proyecto

---
+++ evaluacionjavaobjetos/src/main/java/com/evaluacionjavaobjetos/evaluacionjavaobjetos/EvaluacionjavaobjetosApplication.java
@@ -0,0 +1,13 @@
+package com.evaluacionjavaobjetos.evaluacionjavaobjetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluacionjavaobjetosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluacionjavaobjetosApplication.class, args);
+	}
+
+}
