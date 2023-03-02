0e2a07f4967709fc58bb3399a98847b0bc0561f4
Jesus Garland <jgarlandh@gmail.com>
2023-03-02 03:30:03 -0300

    Creacion funcion "gestionCurso"

--- evaluacionjavaobjetos/src/main/java/com/evaluacionjavaobjetos/evaluacionjavaobjetos/EvaluacionjavaobjetosApplication.java
+++ evaluacionjavaobjetos/src/main/java/com/evaluacionjavaobjetos/evaluacionjavaobjetos/EvaluacionjavaobjetosApplication.java
@@ -1,6 +1,9 @@
 package com.evaluacionjavaobjetos.evaluacionjavaobjetos;
 
 import java.util.ArrayList;
+import java.util.Scanner;
+
+import com.evaluacionjavaobjetos.evaluacionjavaobjetos.models.Alumno;
 
 public class EvaluacionjavaobjetosApplication {
 
@@ -16,7 +19,28 @@ public class EvaluacionjavaobjetosApplication {
 
 	//2.- Guardar un ARRAY de x cantidad de alumnos por curso y que luego los muestre.
 
+	public static void gestionCurso(){
+		Alumno alumnoGestion = new Alumno();
+		ArrayList<Alumno> alumnosCurso = new ArrayList<Alumno>();
+		Scanner input = new Scanner(System.in);
+		System.out.println("Por favor, indíque la cantidad de alumnos:");
+		int numAlumnos = Integer.parseInt(input.nextLine()); 
 
+		for(int i = 1; i <= numAlumnos; i++){
+
+			do {
+				
+				System.out.println("Ingrese alumno nro " + i);
+				System.out.println("Ingrese el nombre del alumno");
+				alumnoGestion.setNombre(input.nextLine());
+
+			} while (i>numAlumnos);
+			alumnosCurso.add(alumnoGestion);
+		}
+
+		System.out.println(alumnosCurso);
+
+	}
 
 
 	//3.- dado un arreglo, obtener la mejor nota, la peor nota y el promedio
